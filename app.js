b803cf54b2b291ef4dcf4e3cba83e963627df889
NarminGale <narmingale@gmail.com>
2021-04-06 20:50:30 +0400

    Completed finding user input letter

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ function findRandomNumber(...a) {
 let randomNumber = findRandomNumber('R', 'P', 'C')
 
 console.log(randomNumber)
+
+let word = 'salam'
+window.onkeypress = function (e) {
+  if (word.includes(e.key)) {
+    console.log(`You choosed right letter: ${e.key}`)
+  } else {
+    console.log(`You didn't choose right letter: ${e.key}`)
+  }
+}
