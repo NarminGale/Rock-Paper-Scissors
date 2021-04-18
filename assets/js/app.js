3b77bbfebd88e2268de985d95c48ba5728a0df79
NarminGale <narmingale@gmail.com>
2021-04-19 00:34:50 +0400

    Added path of js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,87 @@
-function findRandomNumber(...a) {
-  let number = Math.floor(Math.random() * a.length)
-  return a[number]
-}
+// function findRandomNumber(...a) {
+//   let number = Math.floor(Math.random() * a.length)
+//   return a[number]
+// }
 
-let letters = ['r', 'p', 's']
-let userScor = 0
-let compScor = 0
+// let letters = ['r', 'p', 's']
+// let userScor = 0
+// let compScor = 0
 
-window.onkeypress = function (e) {
-  let randomNumber = findRandomNumber('r', 'p', 's')
+// window.onkeypress = function (e) {
+//   let randomNumber = findRandomNumber('r', 'p', 's')
 
-  console.log(`Computer choosed : ${randomNumber}`)
+//   console.log(`Computer choosed : ${randomNumber}`)
 
-  if (letters.indexOf(e.key) !== -1) {
-    console.log(`You choosed : ${e.key}`)
-  } else {
-    console.log(`You cann't : ${e.key}`)
-  }
+//   if (letters.indexOf(e.key) !== -1) {
+//     console.log(`You choosed : ${e.key}`)
+//   } else {
+//     console.log(`You cann't : ${e.key}`)
+//   }
 
-  if (randomNumber === 'r' && e.key === 'p') {
-    userScor = userScor + 1
-    compScor = compScor
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  } else if (randomNumber === 'r' && e.key === 's') {
-    userScor = userScor
-    compScor = compScor + 1
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  } else if (randomNumber === 'r' && e.key === 'r') {
-    userScor = userScor
-    compScor = compScor
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  }
+//   if (randomNumber === 'r' && e.key === 'p') {
+//     userScor = userScor + 1
+//     compScor = compScor
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   } else if (randomNumber === 'r' && e.key === 's') {
+//     userScor = userScor
+//     compScor = compScor + 1
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   } else if (randomNumber === 'r' && e.key === 'r') {
+//     userScor = userScor
+//     compScor = compScor
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   }
 
-  if (randomNumber === 's' && e.key === 'p') {
-    userScor = userScor
-    compScor = compScor + 1
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  } else if (randomNumber === 's' && e.key === 's') {
-    userScor = userScor
-    compScor = compScor
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  } else if (randomNumber === 's' && e.key === 'r') {
-    userScor = userScor + 1
-    compScor = compScor
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  }
+//   if (randomNumber === 's' && e.key === 'p') {
+//     userScor = userScor
+//     compScor = compScor + 1
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   } else if (randomNumber === 's' && e.key === 's') {
+//     userScor = userScor
+//     compScor = compScor
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   } else if (randomNumber === 's' && e.key === 'r') {
+//     userScor = userScor + 1
+//     compScor = compScor
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   }
 
-  if (randomNumber === 'p' && e.key === 'p') {
-    userScor = userScor
-    compScor = compScor
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  } else if (randomNumber === 'p' && e.key === 's') {
-    userScor = userScor + 1
-    compScor = compScor
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  } else if (randomNumber === 'p' && e.key === 'r') {
-    userScor = userScor
-    compScor = compScor + 1
-    console.log(`user scor is: ${userScor}`)
-    console.log(`comp scor is: ${compScor}`)
-  }
+//   if (randomNumber === 'p' && e.key === 'p') {
+//     userScor = userScor
+//     compScor = compScor
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   } else if (randomNumber === 'p' && e.key === 's') {
+//     userScor = userScor + 1
+//     compScor = compScor
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   } else if (randomNumber === 'p' && e.key === 'r') {
+//     userScor = userScor
+//     compScor = compScor + 1
+//     console.log(`user scor is: ${userScor}`)
+//     console.log(`comp scor is: ${compScor}`)
+//   }
+// }
+
+const userStuffs = document.querySelectorAll('.user-stuffs div')
+console.log(userStuffs)
+
+for (let i = 0; i < userStuffs.length; i++) {
+  userStuffs[i].addEventListener('click', function (e) {
+    const classOfDiv = e.currentTarget.classList
+    if (classOfDiv.contains('rock')) {
+      console.log('rock')
+    } else if (classOfDiv.contains('paper')) {
+      console.log('paper')
+    } else if (classOfDiv.contains('scissors')) {
+      console.log('scissors')
+    }
+  })
 }
